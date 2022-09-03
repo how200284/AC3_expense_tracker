e8637c87d94d8d5c44070856155fb704bc609198
how200284 <how200284@gmail.com>
2022-09-03 14:44:06 +0800

    feat:import method-override, add route of edit page & build edit page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -16,6 +17,7 @@ const PORT = process.env.PORT || 3000
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.use(routes)
 
--- routes/modules/expense.js
+++ routes/modules/expense.js
@@ -7,6 +7,7 @@ const Category = require('../../models/category')
 const Record = require('../../models/record')
 
 // set routes
+  // new page
 router.get('/new', (req, res) => {
   Category.find()
     .lean()
@@ -17,6 +18,24 @@ router.post('/', (req, res) => {
   const userId = req.user._id
   const { name, date, amount, category } = req.body
   Record.create({ name, date, category, amount, userId })
+    .then(() => res.redirect('/'))
+    .catch(err => console.error(err))
+})
+
+  // edit page
+router.get('/:recordId/edit', (req, res) => {
+  const recordId = req.params.recordId
+  return Record.findOne({ recordId })
+    .lean()
+    .then(record => res.render('edit', { record }))
+    .catch(err => console.error(err))
+})
+
+router.put('/:recordId', (req, res) => {
+  const UserId = req.user._id
+  const editedInput = req.body
+  const recordId = req.params.recordId
+  return Record.findOneAndUpdate({ UserId }, req.body)
     .then(() => res.redirect('/'))
     .catch(err => console.error(err))
 })
