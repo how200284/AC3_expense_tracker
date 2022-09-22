631abcecce49f58ab6d2c971fe2a8f7ee56783e7
how200284 <how200284@gmail.com>
2022-09-22 12:21:02 +0800

    feat: add category icon & dynamic total amount on home page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -12,17 +12,20 @@ router.get('/', async (req, res) => {
   try {
     const userId = req.user._id
     const categoryId = req.query.categoryId
+    let totalAmount = 0
     let categories = await Category.find({}).lean()
     if (!categoryId) {
-      let records = await Record.find({ userId }).lean()
+      let records = await Record.find({ userId }).lean().populate('categoryId') // add populate() for eager loading
       records.forEach(record => {
         record.date = dayjs(record.date).format('YYYY-MM-DD')
+        totalAmount += record.amount
       })
-      res.render('index', { records, categories })
+      res.render('index', { records, categories, totalAmount })
     } else {
-      let records = await Record.find({ userId, categoryId }).lean()
+      let records = await Record.find({ userId, categoryId }).lean().populate('categoryId') // add populate() for eager loading
       records.forEach(record => {
         record.date = dayjs(record.date).format('YYYY-MM-DD')
+        totalAmount += record.amount
       })
       res.render('index', { records, categories, totalAmount })
     }
